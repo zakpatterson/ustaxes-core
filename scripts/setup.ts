afbc5b372d89cf2a457bae28d5d7bfba8146d44d
Zak Patterson <pattersonzak@gmail.com>
2021-11-21 13:06:32 +0900

    Make schema generation reusable

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,16 +1,27 @@
 import { createGenerator } from 'ts-json-schema-generator'
 import fs from 'fs'
 
-const config = {
-  path: 'src/data/index.ts',
-  tsconfig: 'tsconfig.json',
-  type: '*' // Or <type-name> if you want to generate schema for that one type only
+export const saveSchema = (
+  inFile: string,
+  outFile: string,
+  schemaId: string
+): void => {
+  const config = {
+    schemaId,
+    path: inFile,
+    tsconfig: 'tsconfig.json',
+    type: '*' // Or <type-name> if you want to generate schema for that one type only
+  }
+
+  const schema = createGenerator(config).createSchema(config.type)
+  const schemaString = JSON.stringify(schema, null, 2)
+  fs.writeFile(outFile, schemaString, (err): void => {
+    if (err) throw err
+  })
 }
 
-const outputPath = 'src/data/validation.json'
+const isMain = require.main === module
 
-const schema = createGenerator(config).createSchema(config.type)
-const schemaString = JSON.stringify(schema, null, 2)
-fs.writeFile(outputPath, schemaString, (err): void => {
-  if (err) throw err
-})
+if (isMain) {
+  saveSchema('src/data/index.ts', 'src/data/validation.json', 'ustaxes-forms')
+}
