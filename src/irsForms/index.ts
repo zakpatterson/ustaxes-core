d806d5c6670a473321f93588c2fa6e442262b4ed
Zak Patterson <pattersonzak@gmail.com>
2021-12-03 15:08:03 +0900

    Backport 2020 updates

--- src/irsForms/index.ts
+++ src/irsForms/index.ts
@@ -1,35 +1,56 @@
 import { PDFDocument } from 'pdf-lib'
 import { create1040 } from '../irsForms/Main'
-import { isLeft } from '../util'
+import { Either, isLeft, isRight, right } from '../util'
 import _ from 'lodash'
 import log from '../log'
 import { combinePdfs, getPdfs, PDFDownloader } from '../pdfFiller/pdfHandler'
 import { Information } from '../data'
+import Form from './Form'
+import { F1040Error } from './F1040'
+
+export const insertFormDataToPdfs = async (
+  forms: Form[],
+  downloader: PDFDownloader
+): Promise<PDFDocument[]> => {
+  const pdfs: PDFDocument[] = await Promise.all(
+    forms.map(async (f) => await downloader(`/irs/${f.tag}.pdf`))
+  )
+
+  return getPdfs(_.zipWith(forms, pdfs, (a, b) => [a, b]))
+}
 
 export const create1040PDFs =
   (state: Information) =>
-  async (downloader: PDFDownloader): Promise<PDFDocument[]> => {
+  async (
+    downloader: PDFDownloader
+  ): Promise<Either<F1040Error[], PDFDocument[]>> => {
     if (state.taxPayer !== undefined) {
       const f1040Result = create1040(state)
       // Get data and pdf links applicable to the model state
       if (isLeft(f1040Result)) {
-        return await Promise.reject(f1040Result.left)
+        return Promise.resolve(f1040Result)
       }
 
       const [, forms] = f1040Result.right
 
-      const pdfs: PDFDocument[] = await Promise.all(
-        forms.map(async (f) => await downloader(`/irs/${f.tag}.pdf`))
-      )
-
-      return getPdfs(_.zipWith(forms, pdfs, (a, b) => [a, b]))
+      return right(await insertFormDataToPdfs(forms, downloader))
     }
 
     log.error('Attempt to create pdf with no data, will be empty')
-    return []
+    return right([])
   }
 
 export const create1040PDF =
   (state: Information) =>
-  async (downloader: PDFDownloader): Promise<Uint8Array> =>
-    (await combinePdfs(await create1040PDFs(state)(downloader))).save()
+  async (
+    downloader: PDFDownloader
+  ): Promise<Either<F1040Error[], Uint8Array>> => {
+    const pdfResult = await create1040PDFs(state)(downloader)
+    if (isRight(pdfResult)) {
+      const pdf = await combinePdfs(pdfResult.right)
+      const bytes = await pdf.save()
+      return right(bytes)
+    } else {
+      return Promise.resolve(pdfResult)
+    }
+  }
